f2d11dd2b917166dba225619b0964f754e08acc3
DHANIYEL DARVESH <129174015+DHANIYEL@users.noreply.github.com>
2024-11-14 23:13:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function sendMail(){
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
         message : document.getElementById("message").value,
+        // consol.log("Hi vro")
     }
     emailjs.send("service_9et8mev","template_sm81liq",parms).then(alert("Email Sent"))
 }
